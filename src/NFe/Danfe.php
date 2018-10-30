8f106c36528d4c8552d7e1331574d4ffdb55e1d0
Marlon oliveira barbosa <marlon.academi@gmail.com>
2018-10-30 14:02:06 -0300

    Update Danfe.php

--- src/NFe/Danfe.php
+++ src/NFe/Danfe.php
@@ -1932,7 +1932,7 @@ class Danfe extends Common
         $x = $this->pImpostoDanfeHelper($x, $y, $w, $h, "DESCONTO", "vDesc");
         $x = $this->pImpostoDanfeHelper($x, $y, $w, $h, "OUTRAS DESPESAS", "vOutro");
         
-        if ($this->ide->finNFe == 4){
+        if ($this->ide->getElementsByTagName('finNFe')->item(0)->nodeValue == 4){
 
             $x = $this->pImpostoDanfeHelper($x, $y, $w, $h, "VALOR TOTAL IPI", "vIPI");
 
